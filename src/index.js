507a3e2c3ef7b45bdb7d503563295d6ae3799244
cup0ra <cup0ra@users.noreply.github.com>
2020-03-21 13:46:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+   
+    bracketsConfig = bracketsConfig.map(el => el.join(''));
+    for (let i = 0; i < bracketsConfig.length;){
+        if(str.includes(bracketsConfig[i])){
+            str = str.replace(bracketsConfig[i], '');
+            i = 0;
+            continue;
+        }
+        i += 1;
+    }
+    if (str.length === 0){
+        return true;
+    }else{
+        return false;
+    }
 }
